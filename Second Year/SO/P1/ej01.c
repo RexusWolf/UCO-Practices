9212557427f5903d11616569d891cd71c0334ea4
RexusWolf <francisjmp99@gmail.com>
2019-01-09 17:50:00 +0100

    Commented P1-ej01 (Studying)

--- Second Year/SO/P1/ej01.c
+++ Second Year/SO/P1/ej01.c
@@ -1,15 +1,31 @@
+/*------------------------------------------------------------------------------
+Exercise_1.c
+In this file, a parent process creates 3 different child processes that print
+their own process id (PID). Then the parent process wait until the children end
+and print the PID of the process that has finished and its status.
+------------------------------------------------------------------------------*/
+
+//key_t
 #include <sys/types.h>
+//fork() and getpid()
 #include <unistd.h>
+//exit()
 #include <stdlib.h>
+//wait()
 #include <wait.h>
+//printf()
 #include <stdio.h>
 
 int main(int argc, char const *argv[]) {
 
 
   int status;
-
-    if (! fork()){
+    /*
+    On success, the PID of the child process is returned in the parent,
+    and 0 is returned in the child. On failure, -1 is returned in the parent,
+    no child process is created, and errno is set appropriatel
+    */
+    if (! fork()){ // It's like if (fork() == 0)
       printf("First Child PID = %d\n", getpid() );
       exit(0);
     }
@@ -25,7 +41,7 @@ int main(int argc, char const *argv[]) {
     }
 
     for (int i=0; i<3; i++) {
-      pid_t pid = wait(&status);
+      pid_t pid = wait(&status); // Waits for the process to change its status.
       printf("Child process with PID = %d finished with status: %d\n", pid, WEXITSTATUS(status));
     }
 
