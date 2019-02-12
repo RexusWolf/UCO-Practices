c60e0200472073e7a46e77b21a6c64ac4a58a00f
RexusWolf <francisjmp99@gmail.com>
2019-02-12 18:53:30 +0100

    Finished 'Monomio' operators

--- Second Year/ED/P1/Monomio.cpp
+++ Second Year/ED/P1/Monomio.cpp
@@ -1,4 +1,4 @@
-/*! 
+/*!
    \file  Monomio.cpp
    \brief Fichero que contiene el código de las funciones de la clase Monomio
 */
@@ -16,7 +16,13 @@
 ed::Monomio & ed::Monomio::operator=(ed::Monomio const &m)
 {
 	// COMPLETAR
+			this->setCoeficiente(m.getCoeficiente());
+			this->setGrado(m.getGrado());
 
+			#ifndef NDEBUG
+				assert(std::abs(this->getCoeficiente() - m.getCoeficiente()) < COTA_ERROR);
+				assert(this->getGrado() == m.getGrado());
+			#endif
 
 	// Se devuelve el objeto actual
 	return *this;
@@ -26,7 +32,13 @@ ed::Monomio & ed::Monomio::operator=(ed::Monomio const &m)
 ed::Monomio & ed::Monomio::operator=(double const &x)
 {
 	// COMPLETAR
+			this->setCoeficiente(x);
+			this->setGrado(0);
 
+			#ifndef NDEBUG
+				assert(std::abs(this->getCoeficiente() - x) < COTA_ERROR);
+				assert(this->getGrado() == 0);
+			#endif
 	// Se devuelve el objeto actual
 	return *this;
 }
@@ -42,7 +54,18 @@ ed::Monomio & ed::Monomio::operator=(double const &x)
 ed::Monomio & ed::Monomio::operator+=(ed::Monomio const &m)
 {
 	// COMPLETAR
+	#ifndef NDEBUG
+			assert(this->getGrado() == m.getGrado());
+	#endif
+			double auxcoef = this->getCoeficiente();
+			int auxgrado = this->getGrado();
+			this->setCoeficiente(this->getCoeficiente() + m.getCoeficiente());
 
+	#ifndef NDEBUG
+			assert(std::abs(this->getCoeficiente() - (auxcoef + m.getCoeficiente())) < COTA_ERROR);
+			assert(this->getGrado() == m.getGrado());
+			assert(this->getGrado() == auxgrado);
+	#endif
 
 	// Se devuelve el objeto actual
 	return *this;
@@ -51,6 +74,100 @@ ed::Monomio & ed::Monomio::operator+=(ed::Monomio const &m)
 
 
 // COMPLETAR EL RESTO DE OPERADORES
+ed::Monomio & ed::Monomio::operator-=(ed::Monomio const &m)
+{
+	// COMPLETAR
+	#ifndef NDEBUG
+			assert(this->getGrado() == m.getGrado());
+	#endif
+			double auxcoef = this->getCoeficiente();
+			int auxgrado = this->getGrado();
+			this->setCoeficiente(this->getCoeficiente() - m.getCoeficiente());
+
+	#ifndef NDEBUG
+			assert(std::abs(this->getCoeficiente() - (auxcoef - m.getCoeficiente())) < COTA_ERROR);
+			assert(this->getGrado() == m.getGrado());
+			assert(this->getGrado() == auxgrado);
+	#endif
+
+	// Se devuelve el objeto actual
+	return *this;
+}
+
+ed::Monomio & ed::Monomio::operator*=(ed::Monomio const &m)
+{
+	// COMPLETAR
+
+			double auxcoef = this->getCoeficiente();
+			int auxgrado = this->getGrado();
+			this->setCoeficiente(this->getCoeficiente() * m.getCoeficiente());
+			this->setGrado(this->getGrado() + m.getGrado());
+	#ifndef NDEBUG
+			assert(std::abs(this->getCoeficiente() - (auxcoef * m.getCoeficiente())) < COTA_ERROR);
+			assert(std::abs(this->getGrado() - (auxgrado + m.getGrado())) == 0);
+
+	#endif
+
+	// Se devuelve el objeto actual
+	return *this;
+}
+
+ed::Monomio & ed::Monomio::operator/=(ed::Monomio const &m)
+{
+	// COMPLETAR
+	#ifndef NDEBUG
+			assert(this->getGrado()>=m.getGrado());
+			assert(m.getCoeficiente() != 0.0);
+	#endif
+			double auxcoef = this->getCoeficiente();
+			int auxgrado = this->getGrado();
+			this->setCoeficiente(this->getCoeficiente() / m.getCoeficiente());
+			this->setGrado(this->getGrado() - m.getGrado());
+	#ifndef NDEBUG
+			assert(std::abs(this->getCoeficiente() - (auxcoef / m.getCoeficiente())) < COTA_ERROR);
+			assert(std::abs(this->getGrado() - (auxgrado - m.getGrado())) == 0);
+
+	#endif
+
+	// Se devuelve el objeto actual
+	return *this;
+}
+
+ed::Monomio & ed::Monomio::operator*=(double const &x)
+{
+	// COMPLETAR
+			double auxcoef = this->getCoeficiente();
+			int auxgrado = this->getGrado();
+			this->setCoeficiente(this->getCoeficiente() * x);
+			this->setGrado(this->getGrado() + m.getGrado());
+	#ifndef NDEBUG
+			assert(std::abs(this->getCoeficiente() - (auxcoef * x)) < COTA_ERROR);
+			assert(std::abs(this->getGrado() - auxgrado) == 0);
+
+	#endif
+
+	// Se devuelve el objeto actual
+	return *this;
+}
+
+ed::Monomio & ed::Monomio::operator/=(double const &x)
+{
+	// COMPLETAR
+	#ifndef NDEBUG
+			assert(x != 0.0);
+	#endif
+			double auxcoef = this->getCoeficiente();
+			int auxgrado = this->getGrado();
+			this->setCoeficiente(this->getCoeficiente() / x);
+	#ifndef NDEBUG
+			assert(std::abs(this->getCoeficiente() - (auxcoef / x)) < COTA_ERROR);
+			assert(std::abs(this->getGrado() - auxgrado) == 0);
+
+	#endif
+
+	// Se devuelve el objeto actual
+	return *this;
+}
 
 
 
@@ -66,4 +183,3 @@ ed::Monomio & ed::Monomio::operator+=(ed::Monomio const &m)
 // Funciones auxiliares de la clase Monomio
 
 // COMPLETAR
-
--- Second Year/ED/P1/Monomio.hpp
+++ Second Year/ED/P1/Monomio.hpp
@@ -1,4 +1,4 @@
-/*! 
+/*!
 	\file Monomio.hpp
 	\brief Definición de la clase Monomio
 */
@@ -6,7 +6,7 @@
 #ifndef _MONOMIO_HPP_
 #define _MONOMIO_HPP_
 
-// Para usar la funciones pow y std::abs
+// Para usar la funciones pow y std::abs (valor absoluto)
 #include <cmath>
 
 // Para controlar las pre y post condiciones mediante asertos
@@ -14,38 +14,84 @@
 
 #define COTA_ERROR 1.0e-6  //!< Cota de error para la comparación números reales
 
-// Se incluye la clase Monomio dentro del espacio de nombre de la asigantura: ed
+// Se incluye la clase Monomio dentro del espacio de nombre de la asignatura: ed
 namespace ed
 {
-//!  Definición de la clase Monomio:  \f$ coeficiente \hspace{1ex} X^{grado} \f$ 
+//!  Definición de la clase Monomio:  \f$ coeficiente \hspace{1ex} X^{grado} \f$
 class Monomio
 {
 	//! \name Atributos privados de la clase Monomio
 	private:
 
 	// COMPLETAR
-
+			double coeficiente_;
+			int grado_;
 
 	//! \name Funciones o métodos públicos de la clase Monomio
 	public:
 
 	//! \name Constructores de la clase Monomio
-
 	// COMPLETAR
+			inline Monomio(double coeficiente = 0.0, int grado = 0){
+				// Precondiciones
+				#ifndef NDEBUG
+					assert(grado>=0);
+				#endif
 
+				this->setCoeficiente(coeficiente);
+				this->setGrado(grado);
 
+				// Postcondiciones
+				#ifndef NDEBUG
+					assert(std::abs(this->getCoeficiente() - coeficiente) < COTA_ERROR);
+					assert(this->getGrado() == grado);
+				#endif
+			}
+
+			inline Monomio(const Monomio &m){
+
+				this->setCoeficiente(m.getCoeficiente());
+				this->setGrado(m.getGrado());
+
+				// Postcondiciones
+				#ifndef NDEBUG
+					assert(std::abs(this->getCoeficiente() - m.getCoeficiente()) < COTA_ERROR);
+					assert(this->getGrado() == m.getGrado());
+				#endif
+			}
 
 	//! \name Observadores: funciones de consulta de la clase Monomio
 
 	// COMPLETAR
-
-
+			inline double getCoeficiente() const {return coeficiente_;}
+			inline int getGrado() const {return grado_;}
 
 	//! \name Funciones de modificación de la clase Monomio
 
 	// COMPLETAR
+			inline void setCoeficiente(double x){
 
+				coeficiente_ = x;
 
+				// Postcondiciones
+				#ifndef NDEBUG
+					assert(this->getCoeficiente() == x);
+				#endif
+			}
+			inline void setGrado(int n){
+
+				// Postcondiciones
+				#ifndef NDEBUG
+					assert(n>=0);
+				#endif
+
+				grado_ = n;
+
+				// Postcondiciones
+				#ifndef NDEBUG
+					assert(this->getGrado() == n);
+				#endif
+			}
 
 	/////////////////////////////////////////////////
 
@@ -67,7 +113,9 @@ class Monomio
 
 
 		// COMPLETAR EL RESTO DE OPERADORES
+		Monomio & operator-=(ed::Monomio const &m);
 
+		Monomio & operator*=(ed::Monomio const &m);
 
 	/////////////////////////////////////////////////////////////////////////////////////
 
