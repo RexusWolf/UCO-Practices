d36c8cb616f6d2860ee6a8f9d341ac2810ff0487
RexusWolf <francisjmp99@gmail.com>
2019-03-19 19:19:57 +0100

    Working on resumen2.c

---
+++ Second Year/PAS/P2/resumen2.c
@@ -0,0 +1,62 @@
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+  int status;
+  const int BSIZE = 100;
+  char buf[BSIZE];
+  ssize_t nbytes;
+  int num1, num2;
+  char cad1[8];
+  char cad2[8];
+  char cad[20];
+  char *ptr;
+  int fildes[2];
+  /*
+  On success, the PID of the child process is returned in the parent,
+  and 0 is returned in the child. On failure, -1 is returned in the parent,
+  no child process is created, and errno is set appropriatel
+  */
+  printf("Introduce el primer número entero:\n");
+  scanf("%d", &num1 );
+  printf("Introduce el segundo número entero:\n");
+  scanf("%d", &num2 );
+  sprintf(cad1, "%d", num1);
+  strcat(cad1, ";");
+  sprintf(cad2, "%d", num2);
+  strcat(cad1, cad2);
+  strcat(cad, cad1);
+
+  status = pipe(fildes);
+  if (status == -1){
+    printf("Error al crear tubería.\n");
+    exit(0);
+  }
+
+  switch(fork()){
+    case -1: // Error al hacer fork()
+      break;
+    case 0:
+      close(fildes[1]);
+      nbytes = read(fildes[0], buf, BSIZE);
+      ptr = strtok(cad, ";");
+      printf("%s\n", ptr);
+      //cad2 = cad;
+      close(fildes[0]);
+      exit(EXIT_SUCCESS);
+
+    default:
+      close(fildes[0]);
+      write(fildes[1], cad, 20);
+      close(fildes[1]);
+      exit(EXIT_SUCCESS);
+  }
+
+
+
+
+  return 0;
+}
