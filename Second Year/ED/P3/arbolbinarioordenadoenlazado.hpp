c1d4555bdd6f1afdfe8fe5bd1301636bcd045625
rexuswolf <francisjmp99@gmail.com>
2019-04-09 17:22:54 +0200

    Working on P3...

---
+++ Second Year/ED/P3/arbolbinarioordenadoenlazado.hpp
@@ -0,0 +1,249 @@
+#ifndef __ARBOLBINARIOORDENADO_HPP__
+#define __ARBOLBINARIOORDENADOENLAZADO_HPP__
+
+#include <iostream>
+#include <cassert>
+#include "arbolbinarioordenado.hpp"
+#include "operadornodo.hpp"
+
+using namespace std;
+/*!
+\brief Espacio de nombres para la asignatura Estructura de datos.
+*/
+namespace ed
+{
+	/*!\brief Interfaz para definir un ArbolBinario ordenado.
+
+	Define una interfaz para definir un ArbolBinarioOrdenado.
+
+	\arg G define el campo de información a contener en el arbol.
+	\pre G debe tener definidos los operadores < y ==.
+	*/
+	template<class G>
+	class ArbolBinarioOrdenadoEnlazado:public ArbolBinarioOrdenado<G>
+	{
+	private:
+
+		/*!\brief Define un nodo del arbol binario.*/
+		class NodoArbolBinario
+		{
+		private:
+			G _info;  //Valor informativo almacenado en el nodo
+			NodoArbolBinario * _izquierdo;  //Puntero al hijo izquierdo
+			NodoArbolBinario * _derecho;  //Puntero al hijo derecho
+
+		public:
+			NodoArbolBinario (const G &info)
+			{
+				_info = info;
+				#ifndef NDEBUG
+					assert(getIzquierdo() == NULL);
+					assert(getDerecho() == NULL);
+				#endif
+			}
+
+			NodoArbolBinario (const NodoArbolBinario &n)
+			{
+				this = n;
+				#ifndef NDEBUG
+					assert(getInfo() == n.getInfo());
+				#endif
+			}
+
+			/*!\brief Observadores.*/
+			const G & getInfo() const
+			{
+				return _info;
+			}
+
+			NodoArbolBinario *getIzquierdo() const
+			{
+				return _izquierdo;
+			}
+
+			NodoArbolBinario *getDerecho() const
+			{
+				return _derecho;
+			}
+
+			bool esHoja() const
+			{
+				if((getIzquierdo() != NULL) || (getDerecho() != NULL)) return true;
+				return false;
+			}
+
+			void recorridoPreOrden (OperadorNodo<G> &operador) const
+			{
+				operador.aplicar();
+				_izquierdo->recorridoPreOrden(&operador);
+				_derecho->recorridoPreOrden(&operador);				
+
+			}
+
+			void recorridoPostOrden (OperadorNodo<G> &operador) const
+			{
+				_izquierdo->recorridoPostOrden(&operador);
+				_derecho->recorridoPostOrden(&operador);
+				operador.aplicar();	
+			}
+
+			void recorridoInOrden (OperadorNodo<G> &operador) const
+			{
+				_izquierdo->recorridoInOrden(&operador);
+				operador.aplicar();
+				_derecho->recorridoInOrden(&operador);
+			}
+
+			/*!\brief Modificadores. */
+			void setInfo(const G &info)
+			{
+				_info = info;
+			}
+
+			void setIzquierdo(NodoArbolBinario *n)
+			{
+				_izquierdo = n;
+			}
+
+			void setDerecho(NodoArbolBinario *n)
+			{
+				_derecho = n;
+			}
+
+			NodoArbolBinario & operator=(const NodoArbolBinario &n)
+			{
+				#ifndef NDEBUG
+					assert(getIzquierdo() != n.getIzquierdo());
+					assert(getDerecho() != n.getDerecho());
+				#endif
+				setIzquierdo(n.getIzquierdo());
+				setDerecho(n.getDerecho());
+				#ifndef NDEBUG
+					assert(getIzquierdo() == n.getIzquierdo());
+					assert(getDerecho() == n.getDerecho());
+				#endif
+			}
+
+		}; //Fin clase NodoArbolBinario
+
+		//Implementación de la raíz
+		NodoArbolBinario * _raiz; /*!<La raiz del árbol*/
+		NodoArbolBinario * _actual; /*!<Cursor al nodo actual*/
+		NodoArbolBinario * _padre; /*!<Cursor al nodo actual*/
+
+	public:
+
+		ArbolBinarioOrdenadoEnlazado ()
+		{
+			_raiz = NULL;
+			_actual = NULL;
+			_padre = NULL;
+			#ifndef NDEBUG
+				assert(estaVacio());
+			#endif
+		}
+
+		ArbolBinarioOrdenadoEnlazado (const ArbolBinarioOrdenadoEnlazado<G>& a)
+		{
+			this = a;
+		}
+
+		~ArbolBinarioOrdenadoEnlazado ()
+		{
+			if (not estaVacio())
+			borrarArbol();
+			cout << "Destructor Usado \n";
+		}
+
+		ArbolBinarioOrdenadoEnlazado &operator=(const ArbolBinarioOrdenadoEnlazado& a)
+		{
+			// TODO
+		}
+
+		bool insertar(const G &x)
+		{
+			// TODO
+			return false;
+		}
+
+		void borrarArbol()
+		{
+			// TODO
+		}
+
+		bool borrar()
+		{
+			#ifndef NDEBUG
+				assert(_actual != NULL);
+			#endif
+			// TODO
+			return false;
+		}
+
+		void recorridoPreOrden (OperadorNodo<G> &operador) const
+		{
+			operador.aplicar();
+			getIzquierdo()->recorridoPreOrden(&operador);
+			getDerecho()->recorridoPreOrden(&operador);
+			// REVISAR
+		}
+
+		void recorridoPostOrden (OperadorNodo<G> &operador) const
+		{
+			getIzquierdo()->recorridoPostOrden(&operador);
+			getDerecho()->recorridoPostOrden(&operador);
+			operador.aplicar();
+			// REVISAR
+		}
+
+		void recorridoInOrden (OperadorNodo<G> &operador) const
+		{
+			getIzquierdo()->recorridoInOrden(&operador);
+			operador.aplicar();
+			getDerecho()->recorridoInOrden(&operador);
+			// REVISAR
+		}
+
+		bool buscar(const G& x) const
+		{
+			// TODO
+			return false;
+		}
+
+		bool estaVacio() const
+		{
+			// TODO
+			return false;
+		}
+
+		G raiz() const
+		{
+			#ifndef NDEBUG
+			// ASSERT TO DO
+			#endif
+			return getInfo(_raiz);
+		}
+
+		bool existeActual() const
+		{
+			#ifndef NDEBUG
+			// ASSERT TO DO
+			#endif
+			if(_actual != NULL) return true;
+			return false;
+		}
+
+		G actual() const
+		{
+			#ifndef NDEBUG
+				assert(_actual != NULL);
+			#endif
+			return getInfo(_actual);
+		}
+
+		/*!@}*/
+	};
+
+} //namespace ed
+
+#endif //__ARBOLORDENADO_HPP__
