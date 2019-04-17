174e4d7e08b66c9b08945009369528914fa8e5dd
rexuswolf <francisjmp99@gmail.com>
2019-04-17 18:44:24 +0200

    ejercicio6.sh done

--- Second Year/PAS/P3/ejercicio6.sh
+++ Second Year/PAS/P3/ejercicio6.sh
@@ -1,29 +1,37 @@
 #!/bin/bash
 
-# Comprueba el número de argumentos
-if [ $# -lt 1 ] || [ $# -gt 2 ]; then
-	echo "Uso del programa: ./ejercicio2.sh <directorio> <bytes>"
+# Comprueba los argumentos
+if [ $# -lt 1 ] || [ ! -d $1 ]; then
+	echo "Uso del programa: ./ejercicio6.sh <directorio>"
 	exit 1
 fi
 
-if [ $# -eq 2 ]; then
-	n_bytes=$2
-else 
-	n_bytes=0
+html="$1.html"
+
+if [ -f $html ]; then
+	echo "Borrando fichero html anticuado..."
+	rm -f $html
 fi
 
-for file in $(find $1 -size +"$n_bytes"c -type f -or -size "$n_bytes"c -type f)
-do
-    # Cuenta los caracteres del usuario del fichero
-	CaracteresUser=$(stat -c %U $file | wc -m)
+function listDirectory(){
+	echo "<ul>" >> "$html"
+	for file in $(find "$1" -maxdepth 1)
+	do
+		if [ -d "$file" ] && [ $file != $1 ]; then
+			echo "<li><strong>$file</strong></li>" >> $html
+			listDirectory $file
+		elif [ $file != $1 ]; then
+			echo "<li>$file</li>" >> $html
+		fi
+	done
+	echo "</ul>" >> $html
+}
 
-    # Comprueba si el fichero es ejecutable
-	if [ -x "$file" ]
-	then
-		Exec=1
-	else
-		Exec=0
-	fi
-
-	echo "$(basename $file);$CaracteresUser;$(stat -c '%w;%X;%s;%b;%A' $file);$Exec)"
-done | sort -k 5 -n -t ";"
+echo "</head>" >> $html
+echo "<title>Listado directorios de $1/</title>" >> "$html"
+echo "</head>" >> "$html"
+echo "<body>" >> "$html"
+echo "<h1>Listado directorios de $1/</h1>" >> "$html"
+listDirectory $1
+echo "</body>" >> "$html"
+echo "</html>" >> "$html"
