5021c02baba34feaab68b318cd81278f0be8a2a2
rexuswolf <francisjmp99@gmail.com>
2019-04-23 21:23:05 +0200

    Fixing stuff

--- Second Year/ED/P3/main.cpp
+++ Second Year/ED/P3/main.cpp
@@ -23,81 +23,95 @@ int main()
     a.insertar(generarDatosPersonales());
   }
 
-  cout<<"Elige una opcion: "<<endl;
-  cout<<"1. Insertar más personas en el árbol."<<endl;
-  cout<<"2. Comprobar existencia de una persona en el árbol."<<endl;
-  cout<<"3. Mostrar todos los elementos del árbol."<<endl;
-  cout<<"4. Borrar una persona."<<endl;
-  cout<<"5. Borrar el árbol completo."<<endl;
+  while(option != 0){
+    cout<<"Elige una opcion: "<<endl;
+    cout<<"1. Insertar más personas en el árbol."<<endl;
+    cout<<"2. Comprobar existencia de una persona en el árbol."<<endl;
+    cout<<"3. Mostrar todos los elementos del árbol."<<endl;
+    cout<<"4. Borrar una persona."<<endl;
+    cout<<"5. Borrar el árbol completo."<<endl;
+    cout << "0. Salir\n" << endl;
 
-  cin>>option;
-  switch(option){
+    cin>>option;
+    switch(option){
 
-    // INSERCIÓN DE PERSONAS EN EL ÁRBOL
-    case 1:{
-      cout<<"Introduce numero de personas:"<<endl;
-      cin>>n;
-      for(i = 0; i < n; i++){
-        a.insertar(generarDatosPersonales());
-      }
-    }break;
-
-    // BÚSQUEDA DE PERSONA EN EL ÁRBOL
-    case 2:{
-      Persona persona;
-      cout<<"Introduce el dni, apellido y nombre"<<endl;
-      cin>>persona;
-      if(a.buscar(persona)) cout<< a.actual() <<endl;
-      else cout<<"Persona no encontrada"<<endl;
-    }break;
-
-    // MOSTRADO DE ÁRBOL
-    case 3:{
-      int orden;
-      cout<<"Elige el orden a mostrar:"<<endl;
-      cout<<"1. PreOrden"<<endl;
-      cout<<"2. InOrden"<<endl;
-      cout<<"3. PostOrden"<<endl;
-      cin>>orden;
-      switch(orden){
-        case 1: {
-          a.recorridoPreOrden(op);
-        }break;
-
-        case 2: {
-          a.recorridoInOrden(op);
-        }break;
-
-        case 3: {
-          a.recorridoPostOrden(op);
-        }break;
-        
-        default:
-          cout<<"Opción errónea."<<endl;
-          break;
-      }
-    }break;
-
-    // BORRADO DE UNA PERSONA EN EL ÁRBOL
-    case 4:{
-      Persona persona;
-      cout<<"Introduce el dni, apellido y nombre"<<endl;
-      cin>>persona;
-      if(a.buscar(persona)){
-        if(a.borrar()){
-        cout<<"Borrado satisfactoriamente."<<endl;
+      // INSERCIÓN DE PERSONAS EN EL ÁRBOL
+      case 1:{
+        cout<<"Introduce numero de personas:"<<endl;
+        cin>>n;
+        for(i = 0; i < n; i++){
+          a.insertar(generarDatosPersonales());
         }
-      }
-      else cout<<"Fallo al borrar!"<<endl;
-    }break;
+      }break;
 
-    // BORRADO DEL ÁRBOL
-    case 5:{
-     a.borrarArbol();
-    }break;
-    default:{
-      cout<<"Opción incorrecta."<<endl;
-      break;
+      // BÚSQUEDA DE PERSONA EN EL ÁRBOL
+      case 2:{
+        Persona persona;
+        cout<<"Introduce el dni, apellido y nombre"<<endl;
+        cin>>persona;
+        if(a.buscar(persona)) cout<< a.actual() <<endl;
+        else cout<<"Persona no encontrada"<<endl;
+      }break;
+
+      // MOSTRADO DE ÁRBOL
+      case 3:{
+        int orden;
+        cout<<"Elige el orden a mostrar:"<<endl;
+        cout<<"1. PreOrden"<<endl;
+        cout<<"2. InOrden"<<endl;
+        cout<<"3. PostOrden"<<endl;
+        cin>>orden;
+        switch(orden){
+          case 1: {
+            a.recorridoPreOrden(op);
+          }break;
+
+          case 2: {
+            a.recorridoInOrden(op);
+          }break;
+
+          case 3: {
+            a.recorridoPostOrden(op);
+          }break;
+          
+          default:
+            cout<<"Opción errónea."<<endl;
+            break;
+        }
+      }break;
+
+      // BORRADO DE UNA PERSONA EN EL ÁRBOL
+      case 4:{
+        Persona persona;
+        cout<<"Introduce el dni, apellido y nombre"<<endl;
+        cin>>persona;
+        if(a.buscar(persona)){
+          if(a.borrar()){
+          cout<<"Borrado satisfactoriamente."<<endl;
+          }
+        }
+        else cout<<"Fallo al borrar!"<<endl;
+      }break;
+
+      // BORRADO DEL ÁRBOL
+      case 5:{
+      a.borrarArbol();
+      }break;
+
+      case 0:{
+        cout <<"Saliendo...\n" << RESET;
+      }break;
+
+      default:{
+        cout<<"Opción incorrecta."<<endl;
+        break;
+      }
+
+      if (opt != 0){
+      cout << "Pulse [ENTER] para continuar\n";
+      cin.ignore();
+      cin.get();
+      system("clear");
     }
   }
 }
