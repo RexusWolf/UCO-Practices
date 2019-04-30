d0439fa241004b95e5c56f114e0d1f578cdd037e
rexuswolf <francisjmp99@gmail.com>
2019-04-30 17:34:06 +0200

    Working on P4

--- Second Year/ED/P4/grafo.hpp
+++ Second Year/ED/P4/grafo.hpp
@@ -21,17 +21,63 @@ namespace ed
 	public:
 
 		// constructores
-		// TODO
-
-		// destructor
-		// TODO
-
-		// funciones
-		// TODO
-		void borrarGrafo() {	// cabecera indicada para que compile
-
+		//Crea un nuevo grafo vacío
+		Grafo (){
+			_nodos = {};
+			_lados = {};
+		}
+		// Crea un nuevo grafo inicializando las estructuras para almacenar n nodos
+		Grafo (int n){
+			for(int i=0; i<n; i++){
+				_nodos[i] = NULL;
+			}
+			_lados = new G_Lado*[n];
+		}
+		// Crea un nuevo grafo a partir de otro grafo.
+		Grafo (Grafo &g){
+			_nodos = g._nodos;
+			_lados = g._lados;
 		}
 
+		// destructor (Elimina el grafo)
+		~Grafo(){
+			_nodos = {};
+			_lados = {};
+		}
+
+		// observers
+		G_Lado getLados(){ return _lados;}
+		G_Nodo getNodos(){ return _nodos;}
+		
+		// modififers
+		void setLados(G_Lado lados){ _lados = lados;}
+		void setNodos(G_Nodo nodos){ _nodos = nodos;}
+		// Borra el grafo liberando la memoria
+		void borrarGrafo() {	// cabecera indicada para que compile
+			delete(this);
+		}
+		// Operador de asignación. Operador que copia el grafo “g” en el grafo actual
+		Grafo operador=(Grafo &g){
+			_lados = g._lados;
+			_nodos = g._nodos;
+			return this;
+		}
+
+		void insertNodo(G_Nodo &n){
+			
+		}
+		// Carga un grafo desde fichero
+		bool cargarGrafo(char matrizfile, char nodofile){
+			std::ofstream f;
+			f.open(matrizfile, std:ios::out); // Abrimos los ficheros en modo escritura.
+			f.open(nodofile, std:ios::out);
+			std::istream_iterator<G_Nodo> itnodo;
+			for(itnodo = nodofile.begin(); itnodo = nodofile.end(); itnodo++){
+				;
+			}
+		}
+
+	
 	};
 }
 
